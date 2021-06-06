b95fd54c97b1aa8d749b5a1b5e311827e0dc267a
Fajarullah <frullah12@gmail.com>
2021-06-06 17:37:49 +0700

    add neural network class

---
+++ net.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 8, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(8*8*8, 32)
+        self.fc2 = nn.Linear(32, 2)
+
+    def forward(self, x):
+        out = F.max_pool2d(torch.tanh(self.conv1(x)), 2)
+        out = F.max_pool2d(torch.tanh(self.conv2(out)), 2)
+        out = out.view(-1, 8*8*8)
+        out = torch.tanh(self.fc1(out))
+        out = self.fc2(out)
+        return out
